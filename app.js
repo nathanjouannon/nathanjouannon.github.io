857f3c102e74bce6ae5fb8601152d7402bd96bca
nathanjouannon <82818708+nathanjouannon@users.noreply.github.com>
2022-04-24 19:21:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_items = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
-const header = document.querySelector('.header .container');
+const header = document.querySelector('.header');
 
 hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('active');
@@ -19,7 +19,7 @@ menu_items.forEach( (item) => {
 document.addEventListener('scroll',()=>{
     var scroll_position = window.scrollY;
     if (scroll_position > 300 ) {
-        header.style.backgroundColor = "#29323c";
+        header.style.backgroundColor = "rgba(31, 30, 30, 0.5)";
     }else{
         header.style.backgroundColor= "transparent";
     }
